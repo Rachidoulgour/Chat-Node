1989063d7a5bad8ca18ea0cbb1df6a3373fdfa80
rachid <olgor@hotmail.com>
2020-03-14 20:16:49 +0100

    nodemon added

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const io = require('socket.io')(http);
 app.get('/', (req, res)=>
 res.send('Hola Chat')
 );
-const server = http.listen(3300, ()=>
+const server = http.listen(8080, ()=>
 console.log('Hola desdee el chat node, escuchando desde 3300')
 );
