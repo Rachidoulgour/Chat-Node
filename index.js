4d450e91fb43b25a1723ccaeb7f79cf3f75a4421
rachid <olgor@hotmail.com>
2020-03-14 20:42:14 +0100

    index.ejs added

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res)=>
-res.send('Hola Chat')
+res.render('index')
 );
+
 const server = http.listen(8080, ()=>
 console.log('Hola desdee el chat node, escuchando desde 3300')
 );
