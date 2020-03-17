823e5ac8692f013189b92f004bc79a4a3d18a904
rachid <olgor@hotmail.com>
2020-03-17 19:19:24 +0100

    message and style added

--- index.js
+++ index.js
@@ -17,4 +17,15 @@ console.log('Hola desdee el chat node, escuchando desde 3300')
 
 io.on('connection', (socket)=>{
     console.log("New Client Connected")
+    socket.username = "Guest"
+    socket.on("username_input", (data)=>{
+        socket.username = data.username
+        console.log(data.username)
+    })
+    socket.on("new-message", (data)=>{
+        io.sockets.emit('new-message', {message: data.message, username: socket.username})
+    })
+    socket.on("istyping", (data)=>{
+        socket.broadcast.emit("istyping", {username: socket.username})
+    })
 });
--- public/main.js
+++ public/main.js
@@ -1,5 +1,33 @@
-var socket = io('http://localhost:8080');
-  socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
-  });
+$(function(){
+    var socket = io.connect('http://localhost:8080');
+    var username = $("#username")
+    var register_username =$("#register_username")
+    var message = $("#message")
+    var sendMessage = $("#send_message")
+    var chat = $("#chat")
+    var typing = $("#typing")
+
+    register_username.click(()=>{
+        console.log(username.val())
+        socket.emit('username_input', {username: username.val()})
+    })
+    sendMessage.click(()=>{
+        event.preventDefault();
+        console.log(message.val())
+        socket.emit('new-message', {message:message.val()})
+        console.log(message)
+        message.val('')
+    })
+    socket.on('new-message', (data)=>{
+        chat.append("<p class='message-sended'>"+data.username+': '+data.message+"<p>")
+        typing.html('')
+    })
+    message.bind('keypress', ()=>{
+        socket.emit('istyping')
+    })
+    socket.on('istyping', (data)=>{
+        typing.html("<p><i>"+data.username+" is typing..."+"</i></p>")
+    })
+})
+
+
